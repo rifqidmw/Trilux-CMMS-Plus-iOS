712f2f59aad2c5387f2e237ec66c8ab9f5c19311
upinotuniv <luthfy.rahmani04@gmail.com>
2024-04-21 13:44:17 +0700

    feat: integration work sheet preventive listing page

--- cmmsv2/Modules/PreventiveMaintenanceList/PreventiveMaintenanceListInteractor.swift
+++ cmmsv2/Modules/PreventiveMaintenanceList/PreventiveMaintenanceListInteractor.swift
@@ -1,16 +1,18 @@
-// 
+//
 //  PreventiveMaintenanceListInteractor.swift
 //  cmmsv2
 //
 //  Created by PRO M1 2020 8/256 on 11/03/24.
 //
 
-import Foundation
+import Combine
 
 class PreventiveMaintenanceListInteractor: BaseInteractor {
     
-    // func fetchData(completion: @escaping (Result<ModelResponse, Error>) -> Void) {
-    //    api.requestApi(.exampleEndpoint, completion: completion)
-    // }
+    func getWorkSheetPreventive(limit: Int? = nil,
+                                page: Int? = nil,
+                                engineer: Int? = nil) -> AnyPublisher<PreventiveMaintenanceData, Error> {
+        return api.requestApiPublisher(.workSheetPreventive(limit: limit, page: page, engineer: engineer))
+    }
     
 }
